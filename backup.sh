105ecafb1ba5b44319bddbffcac2ba4e8ed7c68f
Dave Desrochers <dave726@gmail.com>
2014-01-16 22:05:04 -0800

    Now functional for external backup

--- backup.sh
+++ backup.sh
@@ -1,50 +1,58 @@
 #!/bin/sh
-# Assumed to be run as 'root' user
+#
 
 LOGPATH="/var/log/backup/"
+LOG=/dev/stdout
 
-## Network backup variables (using fuse on top of vfat system to get ext2)
 BACKUPDIR="/mnt/backup"
 
-## USB backup variables
-USBDIR=$BACKUPDIR
+## backup variables
 UUID="3a0d8332-8c96-4160-83a0-48e369955c1d"
-OSDIR="$USBDIR/OS/Chunky2"
-MEDDIR="$USBDIR/Media"
+OSDIR="$BACKUPDIR/OS/Chunky2"
+MEDDIR="$BACKUPDIR/Media"
+EXCLUDE_FILE="/etc/backup-exclude.conf"
+
+function usage() {
+  echo 1>&2 Usage: $0 "[-s|--system] OR [-m|--media] OR [-dm|--debugmount] [-du|--umount] [-h|--help]"
+  echo 1>&2 "Specify -s or -m to perform a backup"
+}
+
+# Make sure we're running as root
+if [ "$(id -u)" != "0" ]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
 
 # CLI Argument defaults
-UMOUNT=0;
-BACKUP=0;
-USBMOUNT=0;
-USBUMOUNT=0;
+MOUNT=1;
+UMOUNT=1;
+OSBACKUP=0;
 MEDIABACKUP=0;
-MAINDIR=$USBDIR;
+
 while [ $# -gt 0 ]; do
   case $1 in
-    -u|--usb )
-      USBMOUNT=1;
-      BACKUP=1;
-      USBUMOUNT=1;
+    -s|--system )
+      OSBACKUP=1
+      LOG=$LOGPATH/OS.log
       ;;
     -m|--media )
       MEDIABACKUP=1;
-      LOG="/root/backup_media.log";
+      LOG=$LOGPATH/OS.log
       ;;
     -dm|--debugmount )
-      USBMOUNT=1;
+      UMOUNT=0;
       ;;
     -du|--umount )
-      USBUMOUNT=1
-      ;;
-    -db|--backup )
-      BACKUP=1
+      MOUNT=0;
       ;;
     -h|--help )
-      echo "-u|--usb: Backup filesystem via USB connection.";
-      echo "          performs mount, backup, and unmount.";
+      usage
+      exit 0
       ;;
     *)
       echo "`date`: Error, bad argument: $1, aborting." >> $LOG 2>&1 3>&1;
+      usage
+      exit 1
       ;;
   esac
   shift
@@ -55,38 +63,38 @@ echo "" > $LOG
 echo "-------------------------------------------">> $LOG
 
 ################## First mount everything ##########################
-if [ $USBMOUNT -eq 1 ]; then
-  echo "`date`: /sbin/mount /dev/disk/by-uuid/$UUID $USBDIR" >> $LOG;
-  /sbin/mount /dev/disk/by-uuid/$UUID $USBDIR >> $LOG 2>&1 3>&1;
+if [ $MOUNT -eq 1 ]; then
+  [ -d $BACKUPDIR ] || mkdir -p $BACKUPDIR;
+  echo "`date`: /bin/mount /dev/disk/by-uuid/$UUID $BACKUPDIR" >> $LOG;
+  /bin/mount /dev/disk/by-uuid/$UUID $BACKUPDIR >> $LOG 2>&1 3>&1;
   if [ "$?" != "0" ]; then
-    echo "`date`: ERROR: Could not mount usb device $UUID to $USBDIR" >> $LOG;
+    echo "`date`: ERROR: Could not mount device $UUID to $BACKUPDIR" >> $LOG;
   fi
 fi
 
 ######################## Backup #######################################
-if [ $BACKUP -eq 1 ]; then
-  if [ $MEDIABACKUP -eq 1 ]; then
-    /usr/bin/rsync -avL --delete --delete-excluded --human-readable \
-      --exclude tmp/ --exclude video/mythtv/                      \
-      /mnt/media/* $MEDDIR >> $LOG 2>&1 3>&1 
-    if [ "$?" != "0" ]; then
-      echo "`date`: ERROR: Could not backup Media data" >> $LOG
-    fi
-  else
-    /usr/bin/rsync -av  --delete --delete-excluded --human-readable \
-      --exclude-from /root/os_exclude.txt                         \
-      /*           $OSDIR >> $LOG 2>&1 3>&1
-    if [ "$?" != "0" ]; then
-      echo "`date`: ERROR: Could not backup OS data" >> $LOG
-    fi
+if [ $MEDIABACKUP -eq 1 ]; then
+  [ -d $MEDIADIR ] || mkdir -p $MEDIADIR;
+  /usr/bin/rsync -av --delete --delete-excluded --human-readable \
+    --exclude tmp/ /media/* $MEDDIR >> $LOG 2>&1 3>&1 
+  if [ "$?" != "0" ]; then
+    echo "`date`: ERROR: Could not backup Media data" >> $LOG
+  fi
+elif [ $OSBACKUP -eq 1 ]; then
+  [ -d $OSDIR ] || mkdir -p $OSDIR;
+  /usr/bin/rsync -av  --delete --delete-excluded --human-readable \
+    --exclude-from $EXCLUDE_FILE \
+    /*           $OSDIR >> $LOG 2>&1 3>&1
+  if [ "$?" != "0" ]; then
+    echo "`date`: ERROR: Could not backup OS data" >> $LOG
   fi
 fi
 
-# ################## Last, unmount everything ##########################
-if [ $USBUMOUNT -eq 1 ]; then
-  echo "`date`: /sbin/umount $USBDIR" >> $LOG
-  /sbin/umount $USBDIR  >> $LOG 2>&1 3>&1
-  if [ "$?" != "0" ]; then
-    echo "`date`: ERROR: Could not unmount usb device at $USBDIR" >> $LOG;
-  fi
-fi
+# # ################## Last, unmount everything ##########################
+# if [ $UMOUNT -eq 1 ]; then
+#   echo "`date`: /bin/umount $BACKUPDIR" >> $LOG
+#   /bin/umount $BACKUPDIR  >> $LOG 2>&1 3>&1
+#   if [ "$?" != "0" ]; then
+#     echo "`date`: ERROR: Could not unmount device at $BACKUPDIR" >> $LOG;
+#   fi
+# fi
