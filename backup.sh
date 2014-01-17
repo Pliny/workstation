5165a08b958b431be5940ae32b4ffdce6c9d8ce5
Dave Desrochers <dave726@gmail.com>
2014-01-16 22:05:28 -0800

    Added LGPL comments

--- backup.sh
+++ backup.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
 #
+# COPYRIGHT
+# -----------------
+# Dave S Desrochers 2014. All rights reserved. Licensed software.
+#
+# COPYING PERMISSION
+# -----------------
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Brief Description
+# -----------------
+# Simple backup script separating OS specific files from media files
+# Hardcoded values are defined in the backup variables
+#
 
 LOGPATH="/var/log/backup/"
 LOG=/dev/stdout
