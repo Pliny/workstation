0fd11dfebee2c1469dfa2527db6b129d62adb29a
Dave Desrochers <dave726@gmail.com>
2014-01-18 10:31:08 -0800

    scripts: added config file to backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -26,16 +26,10 @@
 # Hardcoded values are defined in the backup variables
 #
 
-LOGPATH="/var/log/backup/"
+. /etc/config/backup
+
+
 LOG=/dev/stdout
-
-BACKUPDIR="/mnt/backup"
-
-## backup variables
-UUID="3a0d8332-8c96-4160-83a0-48e369955c1d"
-OSDIR="$BACKUPDIR/OS/Chunky2"
-MEDDIR="$BACKUPDIR/Media"
-EXCLUDE_FILE="/etc/backup-exclude.conf"
 
 function usage() {
   echo 1>&2 Usage: $0 "[-s|--system] OR [-m|--media] OR [-dm|--debugmount] [-du|--umount] [-h|--help]"
@@ -101,14 +95,15 @@ fi
 if [ $MEDIABACKUP -eq 1 ]; then
   [ -d $MEDIADIR ] || mkdir -p $MEDIADIR;
   /usr/bin/rsync -av --delete --delete-excluded --human-readable \
-    --exclude tmp/ /media/* $MEDDIR >> $LOG 2>&1 3>&1 
+    --exclude-from $MEDIA_EXCLUDE_FILE \
+    /media/* $MEDDIR >> $LOG 2>&1 3>&1
   if [ "$?" != "0" ]; then
     echo "`date`: ERROR: Could not backup Media data" >> $LOG
   fi
 elif [ $OSBACKUP -eq 1 ]; then
   [ -d $OSDIR ] || mkdir -p $OSDIR;
   /usr/bin/rsync -av  --delete --delete-excluded --human-readable \
-    --exclude-from $EXCLUDE_FILE \
+    --exclude-from $OS_EXCLUDE_FILE \
     /*           $OSDIR >> $LOG 2>&1 3>&1
   if [ "$?" != "0" ]; then
     echo "`date`: ERROR: Could not backup OS data" >> $LOG
